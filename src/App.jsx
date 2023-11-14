5349f291eb73c6cb8e196d57ca0d82381cd7f1c6
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2023-11-14 09:55:20 -0500

    Creación del tablero y acciones

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Square } from "./components/Square";
+
+const turns = { x: 'X', o: 'O' }
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(turns.x)
+
+  const updateBoard = (indexChild) => {
+    const newTurn = turn === turns.x ? turns.o : turns.x
+    let newBoard = [...board]
+    newBoard[indexChild] = turn
+    setBoard(newBoard)
+    setTurn(newTurn)
+  }
+
+  return (
+    <>
+      <main className="board">
+        <h1>Triki</h1>
+        <section className="game">
+          {
+            board.map((_, index) => {
+              return (
+                <Square
+                  key={index}
+                  index={index}
+                  children={_}
+                  updateBoard={updateBoard}
+                />
+              )
+            })
+          }
+        </section>
+        <section className="turn">
+          <Square
+            isSelected={turn === turns.x}
+            children={turns.x}
+          />
+          <Square
+            isSelected={turn === turns.o}
+            children={turns.o}
+          />
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
